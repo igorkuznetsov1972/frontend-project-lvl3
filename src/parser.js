39b918b707807dc5462f4e640fb3c8a4b4e1ff14
Igor <ikk@mail.ru>
2021-04-20 20:45:22 +0300

    remove rss tag check

--- src/parser.js
+++ src/parser.js
@@ -3,21 +3,19 @@ export default (xml) => {
   const feed = parser.parseFromString(xml.data.contents, 'application/xml');
   const parsedFeed = {};
   const parsedItems = [];
-  if (!feed.querySelector('rss') || feed.querySelector('parsererror')) {
-    throw new Error('parseError');
-  } else {
-    parsedFeed.feedTitle = feed.querySelector('title').textContent;
-    parsedFeed.feedDescription = feed.querySelector('description').textContent;
-    Array.from(feed.querySelectorAll('item')).forEach((item) => {
-      const post = { feedId: parsedFeed.feedId };
-      const title = item.querySelector('title');
-      post.postTitle = title.textContent;
-      const description = item.querySelector('description');
-      post.postDescription = description.textContent;
-      const link = item.querySelector('link');
-      post.postUrl = link.textContent;
-      parsedItems.push(post);
-    });
-  }
+  if (feed.querySelector('parsererror')) throw new Error('parseError');
+  parsedFeed.feedTitle = feed.querySelector('title').textContent;
+  parsedFeed.feedDescription = feed.querySelector('description').textContent;
+  Array.from(feed.querySelectorAll('item')).forEach((item) => {
+    const post = { feedId: parsedFeed.feedId };
+    const title = item.querySelector('title');
+    post.postTitle = title.textContent;
+    const description = item.querySelector('description');
+    post.postDescription = description.textContent;
+    const link = item.querySelector('link');
+    post.postUrl = link.textContent;
+    parsedItems.push(post);
+  });
+
   return { parsedFeed, parsedItems };
 };
